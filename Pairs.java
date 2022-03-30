58f436820002983a7c44cbcd3121bdd21367962f
jainulabser <98931333+jainulabser@users.noreply.github.com>
2022-03-30 16:07:12 +0530

    Add files via upload

---
+++ Pairs.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pairs {
+	private int arr[];
+
+	private void noOfPair() {
+		int count, k;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How many numbers want to insert array");
+		count = sc.nextInt();
+		arr = new int[count];
+		System.out.println("Enter " + count + " Values");
+		for (int i = 0; i < count; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("enter number from given array");
+		k = sc.nextInt();
+		sc.close();
+
+		for (int i = 0; i < count; i++) {
+			for (int j = i + 1; j < count; j++) {
+				if ((arr[i] + arr[j]) % k == 0)
+					if(arr[i]<arr[j])
+					System.out.println(arr[i] + " " + arr[j]);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Pairs obj = new Pairs();
+		obj.noOfPair();
+
+	}
+
+}
